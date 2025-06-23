0009bb1155cf9c7f3cc3faad5b3b72dc7398b4d0
Jorge Eduardo <edu0001j@belgium.fhm.de>
2025-06-23 09:54:53 +0100

    adiçao de prometheus e grafana no projeto

--- client-service/src/main/java/br/com/clientservice/config/GatewayHeaderInterceptor.java
+++ client-service/src/main/java/br/com/clientservice/config/GatewayHeaderInterceptor.java
@@ -3,6 +3,7 @@ package br.com.clientservice.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -10,10 +11,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class GatewayHeaderInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
         String fromGateway = request.getHeader("X-From-Gateway");
 
-        if (!"true".equals(fromGateway)) {
+        if (!"true".equals(fromGateway) && !request.getRequestURI().contains("/prometheus")) {
             response.setStatus(HttpStatus.FORBIDDEN.value());
             response.getWriter().write("Access denied: must come through API Gateway.");
             return false;
